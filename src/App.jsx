2d31f73bf3101910f2f2e0fe4e46960ca029daac
junjiezh <zjj476778843@gmail.com>
2024-11-10 17:41:17 +0800

    init

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+import "@mantine/core/styles.css";
+
+import { MantineProvider } from "@mantine/core";
+
+export default function App() {
+  return <MantineProvider>{/* Your app here */}</MantineProvider>;
+}
